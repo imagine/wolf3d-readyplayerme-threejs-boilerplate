cea8523d6335a36b93a29b51d19d2d017b867566
bryanhpchiang <bryanhpchiang@protonmail.com>
2023-04-06 13:34:13 -0700

    yeeet it works kinda without visemes

--- src/index.js
+++ src/index.js
@@ -24,6 +24,55 @@ async function startApplication() {
   const clock = new Clock();
 
   container.appendChild(renderer.domElement);
+  let textField = document.createElement("input");
+  container.appendChild(textField);
+  let button = document.createElement("button");
+  button.innerHTML = "Generate";
+  container.appendChild(button);
+  button.onclick = async () => {
+    console.log("clicked buttton");
+    console.log("generating the audio!");
+    const key = keyField.value;
+    console.log(`this is the key ${key}`);
+
+    console.log(textField.value);
+    const region = "eastus";
+    if (!speech.checkInitialized()) {
+      console.log("Not initialized yet..iitializing..");
+      try {
+        await speech.init(key, region);
+      } catch (e) {
+        console.log("failed to init");
+        console.log(e);
+      }
+      console.log("done init");
+    }
+    console.log("generating audio");
+    try {
+      const audio = await speech.generateAudio(
+        textField.value,
+        "en-US-DavisNeural"
+      );
+      console.log("done generating audio");
+      let visemes = audio.visemeData;
+      console.log(visemes);
+    } catch (e) {
+      console.log("failed to generate audio");
+      console.log(e);
+    }
+  };
+  let keyContainer = document.createElement("div");
+  let instructions = document.createElement("p");
+  instructions.innerHTML = "Please enter your Azure Speech Services key:";
+  keyContainer.appendChild(instructions);
+
+  let keyField = document.createElement("input");
+  keyContainer.appendChild(keyField);
+  // keyField.onchange = (e) => {
+  //   key = e.target.value;
+  // };
+
+  container.appendChild(keyContainer);
 
   createResizeHandler({ renderer, camera });
 
@@ -33,10 +82,10 @@ async function startApplication() {
   const avatar = Avatar.createDefault(renderer);
   scene.withAvatar(avatar);
 
-  await speech.init(key, region);
-  const audio = speech.generateAudio("Hello world", "en-US-DavisNeural");
-  let visemes = audio.visemeData;
-  console.log(visemes);
+  // await speech.init(key, region);
+  // const audio = speech.generateAudio("Hello world", "en-US-DavisNeural");
+  // let visemes = audio.visemeData;
+  // console.log(visemes);
 
   function render() {
     window.requestAnimationFrame(render);
--- src/speech.js
+++ src/speech.js
@@ -1,5 +1,8 @@
 import * as sdk from "microsoft-cognitiveservices-speech-sdk";
+
 import { SpeechConfig } from "microsoft-cognitiveservices-speech-sdk";
+var underscore = require("underscore");
+// import * as underscore from "underscore";
 
 const getSsml = (text, voice) => `
 <speak
@@ -8,7 +11,7 @@ const getSsml = (text, voice) => `
   version="1.0"
   xml:lang="en-us"
 >
-  <voice name="${voice || 'en-US-DavisNeural'}">
+  <voice name="${voice || "en-US-DavisNeural"}">
     <prosody rate="1.0">
       <mstts:viseme type="FacialExpression">
         ${text}
@@ -17,87 +20,81 @@ const getSsml = (text, voice) => `
     </voice>
   </speak>`;
 
+let synthesizer;
+// let client;
 
-let client;
 const checkInitialized = () => {
-    if (client === undefined) throw Error("Azure not initialized");
+  // if (synthesizer === undefined) throw Error("Azure not initialized");
+  return synthesizer !== undefined;
 };
 
 /**
  * Initialization function
  */
-const init = () => {
-    if (process.env.SPEECH_KEY === undefined)
-      throw Error("SPEECH_KEY not defined");
-    if (process.env.SPEECH_REGION === undefined)
-      throw Error("SPEECH_REGION not defined");
-  
-    const speechConfig = sdk.SpeechConfig.fromSubscription(
-      process.env.SPEECH_KEY,
-      process.env.SPEECH_REGION
-    );
+const init = (key, region) => {
+  if (key === undefined) throw Error("SPEECH_KEY not defined");
+  if (region === undefined) throw Error("SPEECH_REGION not defined");
 
-    client = new sdk.SpeechSynthesizer(speechConfig);
-  };
+  const speechConfig = sdk.SpeechConfig.fromSubscription(key, region);
 
-  /**
+  synthesizer = new sdk.SpeechSynthesizer(speechConfig);
+};
+
+/**
  * Audio generation function
  * @param text
  */
 const generateAudio = async (text, voice) => {
-    checkInitialized();
+  checkInitialized();
 
-    const synthesizer = new sdk.SpeechSynthesizer(
-        SpeechConfig.fromSubscription(
-            process.env.SPEECH_KEY,
-            process.env.SPEECH_REGION
-        )
-    );
+  // const synthesizer = new sdk.SpeechSynthesizer(
+  //   SpeechConfig.fromSubscription(
+  //     process.env.SPEECH_KEY,
+  //     process.env.SPEECH_REGION
+  //   )
+  // );
 
-    let visemeData = [];
-    let wordBreakData = [];
+  let visemeData = [];
+  let wordBreakData = [];
 
-    synthesizer.visemeReceived = function (s, e) {
-        if (e.animation) {
-            visemeData.push(
-                ...JSON.parse(e.animation).BlendShapes
-            );
+  synthesizer.visemeReceived = function (s, e) {
+    if (e.animation) {
+      visemeData.push(...JSON.parse(e.animation).BlendShapes);
+    }
+  };
+
+  return new Promise((resolve, reject) => {
+    // Replace '&' with 'and' to avoid SSML errors
+    const ssml = getSsml(underscore.escape(text), voice);
+    synthesizer.speakSsmlAsync(
+      ssml,
+      (result) => {
+        let audio = "";
+
+        if (!!result.audioData) {
+          audio = Buffer.from(result.audioData).toString("base64");
+          resolve({
+            base64Audio: audio,
+            visemeData,
+          });
+        } else if (!!result.errorDetails) {
+          console.log("TTS Error\n", result.errorDetails);
+          reject(result.errorDetails);
+        } else {
+          console.log("TTS Error\n", "No audio data returned");
+          reject("No audio data returned");
         }
-    };
-
-    return new Promise((resolve, reject) => {
-        // Replace '&' with 'and' to avoid SSML errors
-        const ssml = getSsml(underscore.escape(text), voice);
-        synthesizer.speakSsmlAsync(
-            ssml,
-            (result) => {
-                let audio = '';
-
-                if (!!result.audioData) {
-                    audio = Buffer.from(result.audioData).toString("base64");
-                    resolve({
-                        base64Audio: audio,
-                        visemeData,
-                    });
-                }
-                else if (!!result.errorDetails) {
-                    console.log('TTS Error\n', result.errorDetails);
-                    reject(result.errorDetails);
-                }
-                else {
-                    console.log('TTS Error\n', 'No audio data returned');
-                    reject('No audio data returned');
-                }
-            },
-            (error) => {
-                console.log('TTS Error\n', error);
-                reject(error);
-            }
-        );
-    });
+      },
+      (error) => {
+        console.log("TTS Error\n", error);
+        reject(error);
+      }
+    );
+  });
 };
 
 export const speech = {
-    init,
-    generateAudio,
+  init,
+  generateAudio,
+  checkInitialized,
 };
